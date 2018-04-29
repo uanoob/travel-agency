090efdfd402b5bc2591938d314efe78a100e1c2e
uanoob <usnet@ukr.net>
2018-04-29 17:09:02 +0300

    Fix error

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Layout from './hoc/layout';
-import Auth from './hoc/auth';
+// import Auth from './hoc/auth';
 import Home from './components/Home/home';
 import Login from './containers/Admin/login';
 import Register from './containers/Admin/register';
