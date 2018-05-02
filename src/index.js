bcad68c4fb82ef0904df3425a03421ee2adc1f5e
uanoob <usnet@ukr.net>
2018-05-02 16:03:56 +0300

    Remove duplicate code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
